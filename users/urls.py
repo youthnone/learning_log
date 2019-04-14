e693ff3fb08ec910e35baee8acc9cec294d0d492
Julian <sweet882772@126.com>
2019-04-15 00:01:25 +0800

    logout

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 """为应用程序users定义URL模式"""
 
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth.views import LoginView
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     # 登录页面
     path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
+    # 注销
+    # url(r'^logout/$',views.logout_view,name='logout'),
+    #这两种方法都可行
+    path('logout/',views.logout_view,name='logout'),
 ]
